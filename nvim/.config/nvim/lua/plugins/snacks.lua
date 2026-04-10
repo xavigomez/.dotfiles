f3222728c9514eafdbbc67d692ce9e0cf9b7a8bc
Xavier Gómez <xaviergomez@playtomic.io>
2026-04-10 16:08:36 +0200

    feature: exclude .git and node modules from explorer

--- nvim/.config/nvim/lua/plugins/snacks.lua
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -6,10 +6,12 @@ return {
 				explorer = {
 					hidden = true, -- <leader>e
 					ignored = true,
+					exclude = { "node_modules", ".git" },
 				},
 				files = {
 					hidden = true, -- <leader><leader>
 					ignored = true,
+					exclude = { "node_modules", ".git" },
 				},
 			},
 		},
