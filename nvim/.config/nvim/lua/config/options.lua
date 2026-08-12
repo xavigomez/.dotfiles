ef7b7d1e18820d6c6f49403ae1beb2ab0854ba52
Xavier Gómez <codegoonie@gmail.com>
2026-08-12 07:53:47 +0200

    feat(vim): never persist registers to shada file

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -1,5 +1,7 @@
 vim.opt.number = true
 vim.opt.relativenumber = true
+-- Never persist registers to the shada file (they can leak secrets to disk)
+vim.opt.shada = "'100,<0,s10,h"
 vim.opt.expandtab = true
 vim.opt.shiftwidth = 2
 vim.opt.tabstop = 2
